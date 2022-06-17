1b04ed58153dcdb38bf3fdcc8baeb83716afac47
Ankitkr369 <100018606+Ankitkr369@users.noreply.github.com>
2022-06-17 15:23:30 +0530

    Add files via upload

---
+++ pharmeasy cl/script/productspage.js
@@ -0,0 +1,95 @@
+var prodData=JSON.parse(localStorage.getItem("prodData"));
+
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector(".search-input input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink="search.html";
+
+
+
+inputBox.onkeyup = function(e){
+    let userData = e.target.value; 
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = function(){
+            linkTag.setAttribute("href",webLink);
+            linkTag.click();
+        }
+        emptyArray = prodData.filter((data)=>{
+            
+            return data.name.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
+        });
+        emptyArray = emptyArray.map((data)=>{
+  
+            return data = '<li>'+ data.name +'</li>';
+        });
+        searchWrapper.classList.add("active"); 
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); 
+    }
+}
+
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        webLink = "search.html";
+        var out= prodData.filter(function(el,i){
+            return el.name==selectData
+        })
+        console.log(out)
+        localStorage.setItem("searchedItem",JSON.stringify(out[0]))
+        linkTag.setAttribute("href", webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+}
+
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = '<li>'+ userValue +'</li>';
+    }else{
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
+
+// --------------------------------------
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+sliderfun(productContainers,nxtBtn,preBtn)
+
+function sliderfun(productContainer,nxtBtn,preBtn){
+    productContainer.forEach((item, i) => {
+        let containerDimensions = item.getBoundingClientRect();
+        let containerWidth = containerDimensions.width;
+
+        nxtBtn[i].addEventListener('click', () => {
+            item.scrollLeft += containerWidth;
+        })
+
+        preBtn[i].addEventListener('click', () => {
+            item.scrollLeft -= containerWidth;
+        })
+    })
+}
+
+
+
+const customercontainers = [...document.querySelectorAll('#customers')];
+const nb = [...document.querySelectorAll('.nb')];
+const pb = [...document.querySelectorAll('.pb')];
+sliderfun(customercontainers,nb,pb)
